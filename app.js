a26f3b1c9a27d1d6702137a54029aac0ffd92a7b
0xmyrddin <0xmyrddin@pm.me>
2024-05-10 05:10:36 +0200

    project: add express endpoint

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const { fetchProvidersPositionComposition } = require('./src/v3-amm/positions-composition-snapshot')
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.get('/v3-composition/:pool/:block', async (req, res) => {
+  const pool = req.params.pool;
+  const block = req.params.block;
+  const data = await fetchProvidersPositionComposition(pool, block, true)
+
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify(data))
+})
+
+app.get('/', (req, res) => {
+  res.send("/")
+})
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`)
+})
